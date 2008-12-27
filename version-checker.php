cf8ec203b4ebf27a08848be2228934b8e4955e58
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-27 11:50:46 +0000

    bump versions
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/version-checker/trunk@331 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- version-checker.php
+++ version-checker.php
@@ -4,7 +4,7 @@ Plugin Name: Version Checker
 Plugin URI: http://www.semiologic.com/software/wp-tweaks/version-checker/
 Description: Allows to hook into WordPress' version checking API with in a distributed environment.
 Author: Denis de Bernardy
-Version: 1.2 alpha
+Version: 1.2 RC
 Author URI: http://www.getsemiologic.com
 Update Service: http://version.semiologic.com/plugins
 Update Tag: version_checker
