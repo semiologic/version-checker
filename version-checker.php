39dbea5f5bc5fb0c83186e0d885bd75f2d2c606b
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-31 09:48:44 +0000

    use xhtml
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/version-checker/trunk@412 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- version-checker.php
+++ version-checker.php
@@ -31,7 +31,7 @@ if ( is_admin() )
 		function version_checker_warning()
 		{
 			echo '<div class="error">'
-				. '<p>' . 'The Version Checker plugin requires WP 2.7 or later.'
+				. '<p>' . 'The Version Checker plugin requires WP 2.7 or later.' . '</p>'
 				. '</div>' . "\n";
 		} # version_checker_warning()
 		
